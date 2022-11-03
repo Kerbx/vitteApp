bda93123100b58afd270ce2accd1ebebd741d2a7
Kerbx <r3ostat@ya.ru>
2022-11-03 18:56:23 +0300

    Created simple interface for service.

---
+++ vitteApp/IService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace vitteApp
+{
+    [ServiceContract(CallbackContract = typeof(IServiceCallback))]
+    public interface IService
+    {
+        [OperationContract]
+        int Connect();
+        [OperationContract]
+        void Disconnect(int ID);
+        [OperationContract(IsOneWay = true)]
+        void Get(string query);
+    }
+
+    public interface IServiceCallback
+    {
+        [OperationContract]
+        void Post(string answer);
+    }
+}
---
+++ vitteApp/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace vitteApp
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Service : IService
+    {
+        public int Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
