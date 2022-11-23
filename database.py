3e3bdb40ab2e50099bc11dcc59e958dde0b0bca1
Kerbx <r3ostat@ya.ru>
2022-11-23 21:58:38 +0300

    .

--- database.py
+++ database.py
@@ -1,6 +1,18 @@
 import sqlite3
 
 
-connection = sqlite3.connect('students.db')
-cursor = connection.cursor()
+class Database():
+    def __init__(self):
+        self.connection = sqlite3.connect('students.db')
+        self.cursor = self.connection.cursor()
+        
+    def createUser(self, username, password):
+        for i in self.createQuery('select username from students;'):
+            if username == i:
+                return None
+            
+        self.cursor.execute(f'insert into students (username, password) values ({username}, {password});')
+    
+    def createQuery(self, query):
+        return self.cursor.execute(query).fetchall()
 
