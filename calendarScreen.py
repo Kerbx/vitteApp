9900927dcd0af118afc145e537df38391225e6ab
Kerbx <r3ostat@ya.ru>
2022-11-29 19:14:00 +0100

    created new screens.

---
+++ calendarScreen.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('2.1.0')
+
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+
+Builder.load_string("""
+
+""")
+
+
+class CalendarScreen(Screen):
+    pass
