3a74ff55e9c49aae7bccd3d3ca2a487add57cd77
Kerbx <r3ostat@ya.ru>
2022-12-06 09:54:28 +0100

    Created server for file upload and download.

---
+++ server/server.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+import threading
+import os
+
+from serverFuncs import acceptThread
+
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5555
+
+
+sock = socket.socket()
+sock.bind((SERVER_HOST, SERVER_PORT))
+
+print(f'[*] SERVER STARTED ON {SERVER_HOST}:{SERVER_PORT}.')
+
+sock.listen(5)
+while True:
+    try:
+        clientSocket, address = sock.accept()
+        acceptingThread = threading.Thread(target=acceptThread, daemon=True, args=(clientSocket, address))
+        acceptingThread.start()
+    except KeyboardInterrupt:
+        print(f'[!] SERVER TERMINATED.')
+        sys.exit(0)
+        
---
+++ server/serverFuncs.py
@@ -0,0 +1,40 @@
+import socket
+import tqdm
+import os
+
+
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+
+def acceptThread(clientSocket, address):
+    print(f'[*] {address} CONNECTED TO SERVER.')
+    received = clientSocket.recv(BUFFER_SIZE).decode()
+    
+    if 'upload' in received:
+        clientSocket.send('receiving'.encode())
+        received = clientSocket.recv(BUFFER_SIZE).decode()
+        filename, filesize = received.split(SEPARATOR)
+        
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        
+        progress = tqdm.tqdm(range(filesize), f"[*] Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        
+        with open(filename, "wb") as file:
+            while True:
+                bytesRead = clientSocket.recv(BUFFER_SIZE)
+                print(bytesRead)
+                if not bytesRead:    
+                    break
+                file.write(bytesRead)
+                progress.update(len(bytesRead))
+        print(f'[*] FILE FROM {address} UPLOADED.')
+        
+        clientSocket.close()
+        print(f'[*] {address} DISCONNECTED.')
+    elif 'send' in received:
+        pass
+    else:
+        print(f'[*] NO MODE IN MESSAGE FROM {address}. THERE IS IT:\n{received}')
+    
